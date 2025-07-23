775cda947ac75458c152684a4362412f97eb4817
Symbian-Bro <siddhubhai998@gmail.com>
2025-07-23 14:59:57 +0530

    Added logging for invalid captcha requests

--- captcha.lua
+++ captcha.lua
@@ -4,6 +4,8 @@ if ngx.req.get_method() == "POST" then
     ngx.header["Location"] = "/"
     ngx.exit(ngx.HTTP_MOVED_TEMPORARILY)
 else
+    local client_ip = ngx.var.remote_addr
+    ngx.log(ngx.WARN, "Incorrect method used on /captcha-verify from IP: ", client_ip)
     ngx.status = 405
     ngx.say("Method Not Allowed")
     ngx.exit(ngx.HTTP_METHOD_NOT_ALLOWED)
